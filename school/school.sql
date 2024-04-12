37ba890a21df71ea52f579be25bfb7a8b7bf203e
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-04-12 20:18:44 +0000

    guardar1

--- school/school.sql
+++ school/school.sql
@@ -14,6 +14,31 @@ CREATE TABLE students (
   phone_number TEXT
 );
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
+
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
 -- Create the rest of the tables
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  student_id INTEGER,
+  section_id INTEGER
+);
 
-
+--FROM teachers INNER JOIN sections ON teachers.id = sections.teacher_id INNER JOIN courses ON courses.id = sections.course_id
